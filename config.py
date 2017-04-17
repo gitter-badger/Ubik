7395d21467baa7b20646a40bd5e59ed1bff7fcfc
harpribot <harshalpriyadarshi6@gmail.com>
2017-04-16 23:14:25 -0500

    adds configs

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+# Facebook API tokens
+ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
+VERIFY_TOKEN = os.environ["VERIFY_TOKEN"]
+
+# Database information
+DATABASE_NAME = os.environ["DB_NAME"]
+DATABASE_USER = os.environ["DB_USER"]
+DATABASE_PASS = os.environ["DB_PASS"]
